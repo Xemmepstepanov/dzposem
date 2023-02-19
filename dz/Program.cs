a67e0be87a756072a922344781ceee196b6bf3be
Aleksandr <Aleksandr_Stepanov_1995@mail.ru>
2023-02-20 00:13:53 +0300

    Домашнее задание

---
+++ dz/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.Write("Введите любое число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= num; i++) Console.WriteLine($"{Math.Pow(i, 3)}");
+Console.Write(" ");
+
